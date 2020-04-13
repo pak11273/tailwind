10174ab4022f9d753844961d685615ef5e25610a
pak11273 <pak11273@gmail.com>
2020-04-13 18:59:22 -0500

    libzy root to lowercase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import { ContextProvider } from "components"
 import LibzyConfig from "../libzy.config"
-import LibzyRoot from "./LibzyRoot"
+import LibzyRoot from "./libzyroot"
 import React from "react"
 
 // import { LibzyRoot } from "components"
-
-
 
 const App = () => (
   <ContextProvider>
